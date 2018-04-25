14c42eff1fe60e15eb1df674e57ee4717cfc4d94
Mandeep <mspanesar145@gmail.com>
2018-04-25 20:19:45 +0530

    fixed query

--- src/main/java/com/accounting/repository/UserDocumentRepository.java
+++ src/main/java/com/accounting/repository/UserDocumentRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.accounting.UserDocument;
@@ -11,6 +12,6 @@ import com.accounting.UserDocument;
 @Repository
 public interface UserDocumentRepository extends JpaRepository<UserDocument, Long>{
 	
-	@Query("select ud from UserDocument ud where ud.categoryId =:categoryId and ud.subCategoryId =:ud.subCategoryId and ud.contentLinkUrl is null")
-	public List<UserDocument> findByNullContentLinkAndCategoryIdAndSubcategoryId(Long categoryId,Long subCategoryId);
+	@Query("select ud from UserDocument ud where ud.categoryId =:categoryId and ud.subCategoryId =:subCategoryId and ud.contentLinkUrl is null")
+	public List<UserDocument> findByNullContentLinkAndCategoryIdAndSubcategoryId(@Param("categoryId") Long categoryId,@Param("subCategoryId") Long subCategoryId);
 }
